adec5e849937537f55fe448972a748d6e8c31644
Mykyta Lysenko <thepigeonduck@gmail.com>
2021-08-02 02:23:33 +0300

    Added empty table processing

--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -1,16 +1,24 @@
 @props(['data', 'names'])
 
-<table class="min-w-full divide-y divide-gray-200">
-    <thead class="bg-gray-50">
-        <tr>
-            @foreach($names as $name)
-                <x-table.table-heading :text="$name" />
+@if($data->count())
+
+    <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-50">
+            <tr>
+                @foreach($names as $name)
+                    <x-table.table-heading :text="$name" />
+                @endforeach
+            </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200">
+            @foreach($data as $item)
+                <x-table.table-row :item="$item" />
             @endforeach
-        </tr>
-    </thead>
-    <tbody class="bg-white divide-y divide-gray-200">
-        @foreach($data as $item)
-            <x-table.table-row :item="$item" />
-        @endforeach
-    </tbody>
-</table>
+        </tbody>
+    </table>
+
+@else
+
+    <h2 class="text-xl my-2 mx-auto text-gray-500">No data</h2>
+
+@endif
